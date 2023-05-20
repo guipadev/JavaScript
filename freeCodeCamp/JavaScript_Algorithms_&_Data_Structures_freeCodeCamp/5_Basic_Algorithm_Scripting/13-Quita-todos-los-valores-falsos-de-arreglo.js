06dd74ea313c9f273b672b3016634df87c5556e0
yadevom <yaormu@gmail.com>
2023-05-19 22:53:16 -0500

    algoritmos script funcionales js

---
+++ freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/5_Basic_Algorithm_Scripting/13-Quita-todos-los-valores-falsos-de-arreglo.js
@@ -0,0 +1,30 @@
+/*
+Rebote falsy
+
+Quita todos los valores falsos de un arreglo.
+
+Los valores falsos en JavaScript son false, null, 0, "", undefined y NaN.
+
+Sugerencia: Intenta convertir cada valor a booleano.
+
+function bouncer(arr) {
+  return arr;
+}
+
+bouncer([7, "ate", "", false, 9]);
+*/
+
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
+
+console.log(bouncer([7, "ate", "", false, 9])); // [7, 'ate', 9]
+
+// Solution two
+function bouncer2(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) newArray.push(arr[i]);
+  }
+  return newArray;
+}
--- freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/5_Basic_Algorithm_Scripting/14-Devuelve-índice-mas-bajo-de-un-array.js
+++ freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/5_Basic_Algorithm_Scripting/14-Devuelve-índice-mas-bajo-de-un-array.js
@@ -10,44 +10,48 @@ que 1 (índice 0), pero menor que 2 (índice 1).
 
 De esta forma, getIndexToIns([20,3,5], 19) debe devolver 2 porque una vez ordenado el arreglo
 éste se verá así [3,5,20] y 19 es menor que 20 (índice 2) y mayor que 5 (índice 1).
-*/
+
 function getIndexToIns(arr, num) {
-    return arr.filter(val => num > val).length;
+  return num;
 }
 
-console.log(getIndexToIns([40, 60], 50)) // 1
+getIndexToIns([40, 60], 50);
+*/
+function getIndexToIns(arr, num) {
+  return arr.filter((val) => num > val).length;
+}
 
+console.log(getIndexToIns([40, 60], 50)); // 1
 
 //Solution 2
 function getIndexToIns2(arr, num) {
-    arr.sort((a, b) => a - b);
-  
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] >= num)
-        return i;
-    }
-  
-    return arr.length;
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) return i;
+  }
+
+  return arr.length;
 }
 
 //Solution 3
 function getIndexToIns3(arr, num) {
-    // sort and find right index
-    let index = arr
-      .sort((curr, next) => curr - next)
-      .findIndex(currNum => num <= currNum);
-    // Returns index or total length of arr
-    return index === -1 ? arr.length : index;
-  }
-  
-console.log(getIndexToIns3([40, 60], 500)) // 2
+  // sort and find right index
+  let index = arr
+    .sort((curr, next) => curr - next)
+    .findIndex((currNum) => num <= currNum);
+  // Returns index or total length of arr
+  return index === -1 ? arr.length : index;
+}
+
+console.log(getIndexToIns3([40, 60], 500)); // 2
 
 //Solution 4
 function getIndexToIns4(arr, num) {
-    return arr
-      .concat(num)
-      .sort((a, b) => a - b)
-      .indexOf(num);
+  return arr
+    .concat(num)
+    .sort((a, b) => a - b)
+    .indexOf(num);
 }
 
-console.log(getIndexToIns4([1, 3, 4], 2)) // 1
+console.log(getIndexToIns4([1, 3, 4], 2)); // 1

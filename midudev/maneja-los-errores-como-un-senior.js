b66654af5a80e2664b3993b5e93e64a9e2d8227b
yadevom <yaormu@gmail.com>
2023-05-25 07:22:13 -0500

    manejos de errores js

---
+++ midudev/maneja-los-errores-como-un-senior.js
@@ -0,0 +1,63 @@
+// Normalmente creamos los errores así
+/*
+const validateUser = ({ name, age, email } = {}) => {
+  if (!name) throw new Error("name is required");
+  if (!age) throw new Error("age is required");
+  if (!email) throw new Error("email is required");
+
+  // Llamar al aBD para guardar el usuario
+  throw new ConnectionError('database is not available')
+};
+
+// Nos indica el error, pero no de que
+/*
+try {
+  validateUser();
+} catch (err) {
+  console.error(err instanceof Error); // true
+}
+*/
+
+// Una forma adecuada del uso de los errores
+const validateUser = ({ name, age, email } = {}) => {
+  if (!name) throw new ValidationError("name is required");
+  if (!age) throw new ValidationError("age is required");
+  if (!email) throw new ValidationError("email is required");
+
+  // Llamar al aBD para guardar el usuario
+  throw new ConnectionError("database is not available");
+};
+
+// Podemos crear nuestros propios errores en JS
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+
+class ConnectionError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ConnectionError";
+  }
+}
+
+const name = "midu";
+const age = 27;
+const email = "midu@gmail.com";
+
+// Uso en codigo async
+try {
+  validateUser({ name, age, email });
+} catch (e) {
+  console.log(e.name);
+  if (e.name === "ConnectionError") {
+    // retry after a few seconds
+    setTimeout(() => {
+      validateUser({ name, age, email });
+    });
+  }
+}
+
+validateUser();

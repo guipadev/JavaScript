f74619635744975c0de33bf8a18b4b4c367c2b01
yadevom <yaormu@gmail.com>
2023-05-29 06:39:18 -0500

    uso sclice en array

--- freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/7_Functional_Programming/11-devolver-parte-de-un-array-SLICE.js
+++ freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/7_Functional_Programming/11-devolver-parte-de-un-array-SLICE.js
@@ -4,6 +4,7 @@ Devolver parte de un arreglo mediante el método slice
 El método slice devuelve una copia de ciertos elementos de un arreglo. 
 Puede tomar dos argumentos, el primero da el índice de dónde comenzar el corte, 
 el segundo es el índice de dónde terminar el corte (y no es inclusivo). 
+
 Si no se proporcionan los argumentos, el valor predeterminado es comenzar desde el principio
 del arreglo hasta el final, la cual es una manera fácil de hacer una copia de todo el arreglo. 
 El método slice no muta el arreglo original, pero devuelve uno nuevo.
@@ -11,17 +12,29 @@ El método slice no muta el arreglo original, pero devuelve uno nuevo.
 Por ejemplo:
 
 var arr = ["Cat", "Dog", "Tiger", "Zebra"];
+
 var newArray = arr.slice(1, 3);
+
 newArray tendría el valor de ["Dog", "Tiger"].
+
 ---------------------------------------------------------------------------------------------
-Utiliza el método slice en la función sliceArray para retornar parte del arreglo anim 
-dados los índices beginSlice y endSlice. La función debe devolver un arreglo.
+Use el método slice en la función sliceArray para devolver parte de la matriz dados los índices 
+anim proporcionados. La función debe devolver una matriz.
+beginSlice 
+endSlice
+
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
 */
 
 function sliceArray(anim, beginSlice, endSlice) {
-    return anim.slice(beginSlice, endSlice)
+  return anim.slice(beginSlice, endSlice);
 }
 
 var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
 
-console.log(sliceArray(inputAnim, 1, 3)) // ['Dog', 'Tiger']
+console.log(sliceArray(inputAnim, 1, 3)); // ['Dog', 'Tiger']
--- freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/7_Functional_Programming/12-eliminar-elementos-de-arreglo-con-SLICE-en-lugar-de-SPLICE.js
+++ freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/7_Functional_Programming/12-eliminar-elementos-de-arreglo-con-SLICE-en-lugar-de-SPLICE.js
@@ -3,40 +3,54 @@ Remueve elementos de un arreglo usando slice en lugar de splice
 
 Un patrón común al trabajar con arreglos es cuando deseas eliminar elementos y 
 conservar el resto del arreglo. 
+
 JavaScript ofrece el método splice para esto, que toma argumentos para el índice de dónde
-comenzar a eliminar elementos, luego la cantidad de elementos para eliminar. 
+comenzar a eliminar elementos, luego la cantidad de elementos para eliminar.
+
 Si no se proporciona el segundo argumento, el valor predeterminado es eliminar 
 elementos hasta el final. 
+
 Sin embargo, el método splice muta el arreglo original en el que se llama. Por ejemplo:
 
 var cities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
 
 cities.splice(3, 1);
 
-Aquí splice devluelve la "string" London y la elimina del arreglo cities. cities tendrá el valor de ["Chicago", "Delhi", "Islamabad", "Berlin"].
+Aquí splice devluelve la "string" London y la elimina del arreglo cities. 
+cities tendrá el valor de ["Chicago", "Delhi", "Islamabad", "Berlin"].
 
 Como vimos en el último desafío, el método slice no muta el arreglo original, 
 pero devuelve uno nuevo que puede ser guardado en una variable. 
+
 Recuerda que el método slice recibe dos argumentos para indicar el comienzo y el final del segmento
 (el final es no inclusivo) y retorna estos elementos en un nuevo arreglo. 
+
 Usar el método slice en lugar de splice ayuda a prevenir cualquier efecto colateral de mutar un arreglo.
 ---------------------------------------------------------------------------------------------------------
-Reescribe la función nonMutatingSplice usando slice en lugar de splice. 
-Debe limitar el arreglo proporcionado cities a una longitud de 3 y devolver 
-un nuevo arreglo con solo los primeros tres elementos.
+Vuelva a escribir la función nonMutatingSplice utilizando slice en lugar de splice. 
+Debería limitar la matriz cities proporcionada a una longitud de 3 
+y devolver una nueva matriz con solo los tres primeros elementos.
 
-No modifiques el arreglo original proporcionado en la función.
+No cambie la matriz original proporcionada a la función.
+
+function nonMutatingSplice(cities) {
+  // Only change code below this line
+  return cities.splice(3);
+
+  // Only change code above this line
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+
+nonMutatingSplice(inputCities);
 */
 
 function nonMutatingSplice(cities) {
-    // Cambia solo el código debajo de esta línea
-    return cities.slice(0, 3);
-  
-    // Cambia solo el código encima de esta línea
-  }
+  return cities.slice(0, 3);
+}
 
 var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
 
-console.log(nonMutatingSplice(inputCities)) // ['Chicago', 'Delhi', 'Islamabad']
+console.log(nonMutatingSplice(inputCities)); // ['Chicago', 'Delhi', 'Islamabad']
 
-console.log(inputCities) // No hubo cambios ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+console.log(inputCities); // No hubo cambios ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];

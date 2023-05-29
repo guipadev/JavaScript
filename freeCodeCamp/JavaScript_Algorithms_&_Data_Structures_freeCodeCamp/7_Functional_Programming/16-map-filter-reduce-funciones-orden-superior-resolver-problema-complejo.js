b77cfc4a8633d715f63447f431efe65cc2b5a1f7
yadevom <yaormu@gmail.com>
2023-05-29 07:05:20 -0500

    uso reduce

--- freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/7_Functional_Programming/16-map-filter-reduce-funciones-orden-superior-resolver-problema-complejo.js
+++ freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/7_Functional_Programming/16-map-filter-reduce-funciones-orden-superior-resolver-problema-complejo.js
@@ -11,21 +11,29 @@ La función debe devolver un nuevo arreglo que contenga los cuadrados de solamen
 Un ejemplo de un arreglo que contiene números reales es [-3, 4.8, 5, 3, -3.2].
 
 Nota: Tu función no debe usar ningún tipo de bucle for o while o la función forEach().
-*/
 
 const squareList = arr => {
-    return arr
-          .filter(num => num > 0 && num % parseInt(num) === 0)
-          .map(num => Math.pow(num, 2));
+  // Only change code below this line
+  return arr;
+  // Only change code above this line
 };
-  
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
+*/
+
+const squareList = (arr) => {
+  return arr
+    .filter((num) => num > 0 && num % parseInt(num) === 0)
+    .map((num) => Math.pow(num, 2));
+};
+
 const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
 
 console.log(squaredIntegers); // [25, 9]
 
 //Comprobamos que la variable squaredIntegers no cambio
-console.log(squareList)    // [25, 9]
-
+console.log(squareList); // [25, 9]
 
 // Solucion 2
 /*

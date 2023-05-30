7442133c641809cc1f8364a239806b641632ba6d
yadevom <yaormu@gmail.com>
2023-05-30 07:24:13 -0500

    uso curry funcion a otra funcion como argumento

--- freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/7_Functional_Programming/24-Introduccion-al-Curry-&-Aplicacion-Parcial.js
+++ freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/7_Functional_Programming/24-Introduccion-al-Curry-&-Aplicacion-Parcial.js
@@ -2,6 +2,7 @@
 Introducción a la currificación y a la aplicación de funciones parciales
 
 La aridad de una función es el número de argumentos que requiere. 
+
 Currificar una función significa convertir una función de N aridades a N funciones de 1 aridad.
 
 En otras palabras, reestructura una función, por lo que toma un argumento, 
@@ -25,7 +26,8 @@ curried(1)(2) devolverá 3.
 
 Esto es útil en tu programa si no puedes proporcionar todos los argumentos a una función al mismo tiempo. 
 Puedes guardar la llamada a cada función dentro de una variable, la cual mantendrá la referencia de la 
-función devuelta que toma el siguiente argumento cuando este disponible. 
+función devuelta que toma el siguiente argumento cuando este disponible.
+
 Aquí hay un ejemplo utilizando la función currificada del ejemplo anterior:
 
 var funcForY = curried(1);
@@ -34,6 +36,7 @@ console.log(funcForY(2)); // 3
 
 Similarmente, la aplicación de una función parcial puede describirse como aplicar algunos 
 argumentos a la función al mismo tiempo y devolviendo una función que se aplica a más argumentos. 
+
 A continuación un ejemplo:
 
 function impartial(x, y, z) {
@@ -45,26 +48,32 @@ var partialFn = impartial.bind(this, 1, 2);
 partialFn(10); // 13
 ----------------------------------------------------------------------------------------------------
 Completa el cuerpo de la función add para que use currificación para añadir parámetros x, y, y z.
+
+function add(x) {
+  // Only change code below this line
+}
+
+add(10)(20)(30);
 */
 
 function add(x) {
-    return function(y) {
-        return function(z) {
-          return x + y + z;
-        }
-    }
+  return function (y) {
+    return function (z) {
+      return x + y + z;
+    };
+  };
 }
 
-console.log(add(10)(20)(30))    // 60
+console.log(add(10)(20)(30)); // 60
 
 //Solution 2
 
 function add2(x) {
-    return function(y) {
-        return function(z) {
-          return x + y + z;
-        }
-    }
+  return function (y) {
+    return function (z) {
+      return x + y + z;
+    };
+  };
 }
 
-console.log(add2(10)(20)(30))   // 60
+console.log(add2(10)(20)(30)); // 60

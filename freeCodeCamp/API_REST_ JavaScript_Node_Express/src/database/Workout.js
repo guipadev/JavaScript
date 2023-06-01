95670d20b96b75a2a48e9ae6fb057a938d97d936
yadevom <yaormu@gmail.com>
2023-06-01 07:01:48 -0500

    metodos bd capa acceso a datos

--- freeCodeCamp/API_REST_ JavaScript_Node_Express/src/database/Workout.js
+++ freeCodeCamp/API_REST_ JavaScript_Node_Express/src/database/Workout.js
@@ -13,8 +13,18 @@ const DB = require("./db.json");
 
 const { saveToDatabase } = require("./utils");
 
+// métodos de base de datos dentro de la capa de acceso a datos
+
 const getAllWorkouts = () => {
   return DB.workouts;
+};
+
+const getOneWorkout = (workoutId) => {
+  const workout = DB.workouts.find((workout) => workout.id === workoutId);
+  if (!workout) {
+    return;
+  }
+  return workout;
 };
 
 const createNewWorkout = (newWorkout) => {
@@ -28,8 +38,38 @@ const createNewWorkout = (newWorkout) => {
   return newWorkout;
 };
 
+const updateOneWorkout = (workoutId, changes) => {
+  const indexForUpdate = DB.workouts.findIndex(
+    (workout) => workout.id === workoutId
+  );
+  if (indexForUpdate === -1) {
+    return;
+  }
+  const updatedWorkout = {
+    ...DB.workouts[indexForUpdate],
+    ...changes,
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+  };
+  DB.workouts[indexForUpdate] = updatedWorkout;
+  saveToDatabase(DB);
+  return updatedWorkout;
+};
+
+const deleteOneWorkout = (workoutId) => {
+  const indexForDeletion = DB.workouts.findIndex(
+    (workout) => workout.id === workoutId
+  );
+  if (indexForDeletion === -1) {
+    return;
+  }
+  DB.workouts.splice(indexForDeletion, 1);
+  saveToDatabase(DB);
+};
+
 module.exports = {
   getAllWorkouts,
-
   createNewWorkout,
+  getOneWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
 };

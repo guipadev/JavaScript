bcc8d61ee973160b4dcc86b881b635d9b8522b03
yadevom <yaormu@gmail.com>
2023-06-01 07:14:11 -0500

    sumar todos los numeros pares finobacci

--- freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/8_Intermediate_Algorithm_Scripting/12-Suma-todos-los-numeros-impares-de-Fibonacci.js
+++ freeCodeCamp/JavaScript_Algorithms_&_Data_Structures_freeCodeCamp/8_Intermediate_Algorithm_Scripting/12-Suma-todos-los-numeros-impares-de-Fibonacci.js
@@ -17,3 +17,22 @@ function sumFibs(num) {
 
 sumFibs(4);
 */
+
+function sumFibs(num) {
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+let rta = sumFibs(4);
+
+console.log(rta); // 5

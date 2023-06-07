d35f9ca4cbc0a71841a1bd69cb5f25e10237fcc6
yadevom <yaormu@gmail.com>
2023-06-07 07:46:42 -0500

    tarea programacion funcional meta

--- Meta-Front-End-Developer/2-Programming-with-JavaScript/3-Programacion-Funcional/3-Tarea-programacion-functional/functionalprogramming.js
+++ Meta-Front-End-Developer/2-Programming-with-JavaScript/3-Programacion-Funcional/3-Tarea-programacion-functional/functionalprogramming.js
@@ -1,15 +1,35 @@
-// Task 1: Build a function-based console log message generator
-function consoleStyler(color, fondo, tamanoFuente, texto) {
-  let mensaje = "%c" + texto;
-  let estilo = `color: ${color};`;
-  estilo += `fondo: ${fondo};`;
+// Tarea 1: Crear función consoleStyler
+function consoleStyler(color, background, fontSize, txt) {
+  let message = "%c" + txt;
+  let style = `color: ${color};`;
+  style += `background: ${background};`;
+  style += `font-size: ${fontSize};`;
+
+  console.log(message, style);
 }
 
-// Task 2: Build another console log message generator
-function celebrateStyler() {}
+// Tarea 2: Crear función celebrateStyler
+function celebrateStyler(reason) {
+  let fontStyle = "color: tomato; font-size: 50px";
 
-// Task 3: Run both the consoleStyler and the celebrateStyler functions
+  if (reason === "birthday") {
+    console.log("%cHappy birthday", fontStyle);
+  } else if (reason === "champions") {
+    console.log("%cCongrats on the title!", fontStyle);
+  } else {
+    console.log(message, style);
+  }
+}
 
-// Task 4: Insert a congratulatory and custom message
-function styleAndCelebrate() {}
-// Call styleAndCelebrate
+// Tarea 3: Ejecutar las funciones consoleStyler y celebrateStyler
+consoleStyler("#1d5c63", "#ede6db", "40px", "Congrats!");
+celebrateStyler("birthday");
+
+// Tarea 4: Insertar un mensaje de felicitación y personalizado
+function styleAndCelebrate(color, background, fontSize, txt, reason) {
+  consoleStyler(color, background, fontSize, txt);
+  celebrateStyler(reason);
+}
+
+// Invocar la nueva función
+styleAndCelebrate("ef7c8e", "fae8e0", "30px", "You made it!", "champions");

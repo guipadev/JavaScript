6709bbe8cd974d6713e504c13c63535a8ec6a703
yadevom <yaormu@gmail.com>
2023-06-08 07:55:00 -0500

    clases en js

---
+++ Meta-Front-End-Developer/2-Programming-with-JavaScript/3-Programacion-Funcional/6-Object-Oriented-Programming-principles.js
@@ -0,0 +1,26 @@
+/*
+Principios de la Programación Orientada a Objetos
+
+Los beneficios de la programación orientada a objetos
+Hay muchos beneficios al usar el paradigma de programación orientada a objetos (POO).
+
+OOP ayuda a los desarrolladores a imitar la relación entre objetos en el mundo real. 
+En cierto modo, te ayuda a razonar sobre las relaciones entre las cosas en tu software, 
+tal como lo harías en el mundo real. 
+Por lo tanto, OOP es un enfoque efectivo para encontrar soluciones en el código que escribe. 
+OOP también:
+
+- Le permite escribir código modular,
+- Hace que su código sea más flexible y 
+- Hace que su código sea reutilizable.
+
+Los principios de la programación orientada a objetos
+Los cuatro principios fundamentales de la programación orientada a objetos son 
+herencia, encapsulación, abstracción y polimorfismo. 
+
+Lo que hay que recordar acerca de los Objetos es que existen en una estructura jerárquica. 
+Lo que significa que la base original o superclase para todo es la clase Object, 
+todos los objetos se derivan de esta clase. 
+Esto nos permite utilizar el método Object.create(). 
+para crear o instanciar objetos de nuestras clases.
+*/
